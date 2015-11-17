fbe5b9a1721e580c1ccec49d35ad28c9a7637374
Denis <deniskozlov@live.ru>
2015-11-17 16:04:12 +0300

    hello world

--- src/com/sprojects/myfirstjavaapp/Main.java
+++ src/com/sprojects/myfirstjavaapp/Main.java
@@ -3,6 +3,7 @@ package com.sprojects.myfirstjavaapp;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        System.out.print("Hello worls");
     }
 }
